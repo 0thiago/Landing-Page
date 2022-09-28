32ae3a0ed82e3005e42705f45558714e720b80fd
Thiago soares <104157930+0thiago@users.noreply.github.com>
2022-09-28 17:49:45 -0300

    cleaning code, adding loading bar on product grid load & sharping css

--- js/main.js
+++ js/main.js
@@ -1,41 +1,38 @@
-
-// It validates input data from help-algorithm section form
-
-const ValidateHelpAlgoForm = {
-
+const validateHelpAlgoForm = {
+  
   init: function () {
-
     this.cacheSelectors()
     this.bindEvents()
-
   },
 
   cacheSelectors: function () {
-
     this.$sendButton = document.querySelector('#sendButton')
     this.$form = document.querySelector('#form')
     this.$inputName = document.querySelector('#inputName')
     this.$inputEmail = document.querySelector('#inputEmail')
     this.$inputCPF = document.querySelector('#inputCPF')
-    this.$inputGenre = document.querySelector('[name="helpAlgoForm_genre"') 
-    this.$invalidField = document.querySelector('.invalid-field')    
-
+    this.$invalidField = document.querySelector('.invalid-field')
   },
 
   bindEvents: function () {
-
     const self = this
 
     this.$sendButton.onclick = self.Events.submit
 
     this.$form.onsubmit = self.Events.validateData.bind(this)
 
+    this.$inputCPF.onkeydown = self.Events.CPFonlyNumber.bind(this)
   },
 
   Events: {
+    CPFonlyNumber: function (event) {
+      if (isNaN(event.key) === true) {
+        return false
+      }
+    },
 
     validateData: function (event) {
-      const self = ValidateHelpAlgoForm
+      const self = validateHelpAlgoForm
 
       event.preventDefault()
 
@@ -49,79 +46,72 @@ const ValidateHelpAlgoForm = {
 
       if (regexName.test(self.$inputName.value) == false) {
         self.$hasError = true
-
         self.$inputName.previousElementSibling.innerHTML = ' Informações Incorretas!'
-        self.$inputName.classList.add('invalid-input')        
-        
-      } else {        
+        self.$inputName.classList.add('invalid-input')
+
+      } else {
         self.$inputName.previousElementSibling.innerHTML = ''
         self.$inputName.classList.remove('invalid-input')
-        
       }
 
       if (regexEmail.test(self.$inputEmail.value) == false) {
         self.$hasError = true
-
         self.$inputEmail.previousElementSibling.innerHTML = ' Informações Incorretas!'
-        self.$inputEmail.classList.add('invalid-input')      
-
-      } else {        
+        self.$inputEmail.classList.add('invalid-input')
+      } else {
         self.$inputEmail.previousElementSibling.innerHTML = ''
         self.$inputEmail.classList.remove('invalid-input')
-
       }
 
       if (regexCPF.test(self.$inputCPF.value) == false) {
         self.$hasError = true
-
         self.$inputCPF.previousElementSibling.innerHTML = ' Informações Incorretas!'
         self.$inputCPF.classList.add('invalid-input')
-        
-  
-      } else {        
+      } else {
         self.$inputCPF.previousElementSibling.innerHTML = ''
         self.$inputCPF.classList.remove('invalid-input')
-
       }
 
-      if (!self.$hasError){
+      if (!self.$hasError) {
         self.$form.submit()
-        
       }
-
     }
   }
 }
 
-ValidateHelpAlgoForm.init()
+validateHelpAlgoForm.init()
 
-// It gets api data, builds structure and behavior of the Products section
-const ProductsSection = {
+const productsSection = {
 
   init: function () {
-
     this.cacheSelectors()
     this.bindEvents()
     this.getProducts()
+  },
 
+  displayLoading: function () { 
+    this.$loadingBar.style.display = "flex";
+  },
+
+  hideLoading: function () {
+    this.$loadingBar.style.display = "none";
   },
 
   productHTMLStructure: function (product) {
-  
     return `
       <div id="productContent" class="products__item">
-          <img id="productImg" class="products__item-img" src="${product.image}" alt="${product.name} Picture" >
+          <div class="products__item-img"><img id="productImg" class="" src="${product.image}" alt="${product.name} Picture" >
+          </div>
           <div class="products__item-desc">
             <p id="productName" class="products__item-desc-name">${product.name}</p>
             <p id="productInfo" class="products__item-desc-info">${product.description}</p>
-            <p id="productOldPrice" class="products__item-desc-old-price">de: R$${product.oldPrice},99</p>
-            <p id="productNewPrice"  class="products__item-desc-new-price">Por: R$${product.price},99</p>
-            <p id="productInstalments" class="products__item-desc-instalments">ou ${product.installments.count}x de R$${product.installments.value.toString().replace(".", ",")}9</p>
+            <p id="productOldPrice" class="products__item-desc-old-price">de: R$ ${product.oldPrice}</p>
+            <p id="productNewPrice"  class="products__item-desc-new-price">Por: R$ ${product.price}</p>
+            <p id="productInstalments" class="products__item-desc-instalments">ou ${product.installments.count}x de R$ ${product.installments.value}</p>
             <button class="products__item-desc-buy-button button">Comprar</button>
           </div>
         </div>
       `
-
   },
 
   convertFromJSON: function (response) {
@@ -129,13 +119,10 @@ const ProductsSection = {
   },
 
   fillProducts: function (response) {
-
     response.products.forEach((product) => {
-
       this.$productContainer.innerHTML += this.productHTMLStructure(product);
-
     })
-
+    this.hideLoading()
   },
 
   showError: function () {
@@ -143,17 +130,16 @@ const ProductsSection = {
   },
 
   getProducts: function () {
+    this.displayLoading()
 
     fetch(`https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${this.$pageNum}`, { method: 'GET' })
       .then(this.convertFromJSON)
       .then(this.fillProducts.bind(this))
       .catch(this.showError.bind(this))
-
   },
 
   cacheSelectors: function () {
-
-    
+    this.$loadingBar = document.querySelector('#loadingBar')
     this.$productContainer = document.querySelector('#productsContainer')
     this.$productContent = document.querySelector('#productContent')
     this.$productImg = document.querySelector('#productImg')
@@ -164,67 +150,50 @@ const ProductsSection = {
     this.$productInstallments = document.querySelector('#productInstallments')
     this.$moreProductsButton = document.querySelector('#moreProductsButton')
     this.$pageNum = 1
-
-
   },
 
   bindEvents: function () {
+    const self = productsSection
 
-    const self = ProductsSection
-
-    this.$moreProductsButton.onclick = self.Events.showMoreProducts_button_click.bind(this)
-
-
+    this.$moreProductsButton.onclick = self.Events.showMoreProductsButtonClick.bind(this)
   },
 
   Events: {
-
-    showMoreProducts_button_click: function () {
-
+    showMoreProductsButtonClick: function () {
       this.$pageNum++
       this.getProducts()
-
     }
   }
 }
 
-ProductsSection.init()
+productsSection.init()
+const shareForm = {
 
-// It validates input data from share section form
-const shareForm = {  
-
-  init: function(){
-
+  init: function () {
     this.cacheSelectors()
     this.bindEvents()
-
   },
 
-  cacheSelectors: function(){
-
+  cacheSelectors: function () {
     this.$form = document.querySelector('#shareForm')
     this.$inputName = document.querySelector('#inputFriendsName')
     this.$inputEmail = document.querySelector('#inputFriendsEmail')
     this.$sendButton = document.querySelector('#shareFormSendButton')
 
-
   },
 
-  bindEvents: function(){
-
+  bindEvents: function () {
     const self = this
 
     this.$sendButton.onclick = self.Events.submit
 
     this.$form.onsubmit = self.Events.validateData.bind(this)
-
   },
 
   Events: {
-
     validateData: function (event) {
       const self = shareForm
-      
+
       event.preventDefault()
 
       self.$hasError = false
@@ -235,38 +204,26 @@ const shareForm = {
 
       if (regexName.test(self.$inputName.value) == false) {
         self.$hasError = true
-
         self.$inputName.previousElementSibling.innerHTML = ' Informações Incorretas!'
-        self.$inputName.classList.add('invalid-input')        
-        
-      } else {        
+        self.$inputName.classList.add('invalid-input')
+      } else {
         self.$inputName.previousElementSibling.innerHTML = ''
         self.$inputName.classList.remove('invalid-input')
-        
       }
 
       if (regexEmail.test(self.$inputEmail.value) == false) {
         self.$hasError = true
-
         self.$inputEmail.previousElementSibling.innerHTML = ' Informações Incorretas!'
-        self.$inputEmail.classList.add('invalid-input')      
-
-      } else {        
+        self.$inputEmail.classList.add('invalid-input')
+      } else {
         self.$inputEmail.previousElementSibling.innerHTML = ''
         self.$inputEmail.classList.remove('invalid-input')
-
       }
 
-      console.log(self.$hasError)
-
-      if (!self.$hasError){
+      if (!self.$hasError) {
         self.$form.submit()
-        
       }
-
     }
-
   }
 }
-
 shareForm.init()
